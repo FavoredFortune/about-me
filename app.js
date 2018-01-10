2581c07601b45bbefd5f17701a7510ca2bf7ffdf
Suzanne Richman <suzanne@favoredfortune.com>
2018-01-10 11:08:16 -0800

    updated HTML and added images

---
+++ app.js
@@ -0,0 +1,34 @@
+//help catch errors right away with 'use strict' BEST PRACTICE
+'use strict';
+
+//var userName = prompt ('Hi , what is your name?');
+//console.log ('The user\s name is ' + userName);
+//alert ('Hi '+ userName + "!" );
+
+//var pets = parseInt(prompt('How many pets do you think I have?'));
+//PSEUDOCODE
+//if user guesses the correct number {
+//  tell the user they were correct} 
+//  else{tell the user they were wrong}
+//if (pets === 2){ 
+//    alert('That is correct!');
+//}   else { 
+//    alert('No. That is incorrect.');
+//}
+
+//Question about age
+//var soozsAge = prompt ('Do you think I\'m older than 40? Please respond with Y or N').toUpperCase ();
+
+//possible answers: yes, no, YES, NO, Yes, No, y, n, Y, N
+
+//if (soozsAge = 'Y') {
+//    alert ('Yes I\m older than 40.');
+//}   else (soozsAge === 'N') 
+//{   alert ('Thank you, I am flattered.');
+//}   else  {
+//    alert('Please enter Y or N!');
+//}
+
+//ARRAYS- collections, store more than one value in a variable as an array, data structure
+
+//var arrayofDogs = ['Gary','Charlotte', 'Demi', 'Bird', 'Mr. Darcy'];
