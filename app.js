1bfced3f0ec38bd4b1997f57e7004fbf58676b92
Suzanne Richman <suzanne@favoredfortune.com>
2018-01-10 12:25:07 -0800

    adding final CSS and interim JS files

--- app.js
+++ app.js
@@ -1,34 +1,79 @@
 //help catch errors right away with 'use strict' BEST PRACTICE
 'use strict';
 
-//var userName = prompt ('Hi , what is your name?');
-//console.log ('The user\s name is ' + userName);
-//alert ('Hi '+ userName + "!" );
+//Greeting
+var userName = prompt ('Hi , what is your name?');
+console.log ('The user\s name is ' + userName);
+alert ('Hi '+ userName + "!" );
 
-//var pets = parseInt(prompt('How many pets do you think I have?'));
-//PSEUDOCODE
-//if user guesses the correct number {
-//  tell the user they were correct} 
-//  else{tell the user they were wrong}
-//if (pets === 2){ 
-//    alert('That is correct!');
-//}   else { 
-//    alert('No. That is incorrect.');
-//}
+//Question about number of pets
+var pets = parseInt(prompt('How many pets do you think I have?'));
+console.log ('userName' + ' guessed '+ 'pets');
+
+if (pets === 2){ 
+    alert('That is correct!');
+}   else { 
+    alert('No. That is incorrect.');
+}
 
 //Question about age
-//var soozsAge = prompt ('Do you think I\'m older than 40? Please respond with Y or N').toUpperCase ();
+var soozsAge = prompt ('Do you think I\'m older than 40? Please respond with Y or N').toUpperCase ();
+console.log ('The user thinks that Sooz is ' + soozsAge + 'than 40.');
 
-//possible answers: yes, no, YES, NO, Yes, No, y, n, Y, N
+if (soozsAge = 'Y') {
+    alert ('Yes I\'m older than 40.');
+}   else if (soozsAge === 'N') 
+{   alert ('Thank you, I am flattered.');
+}   
+    else  {
+    alert('Please enter Y or N!');
+}
 
-//if (soozsAge = 'Y') {
-//    alert ('Yes I\m older than 40.');
-//}   else (soozsAge === 'N') 
-//{   alert ('Thank you, I am flattered.');
-//}   else  {
-//    alert('Please enter Y or N!');
-//}
+//Question about activities
+var soozKnit= prompt ('Do I like to knit? Please respond with Y or N.').toUpperCase ();
+console.log ('the user thinks that Sooz ' + soozKnit + ' to knit.');
+
+if (soozKnit= 'Y') {
+    alert ('Yes I do like to knit!');
+
+}   else if (soozKnit=== 'N') 
+{   alert ('Nope! See the pictures below.');
+}   
+    else 
+{   alert('Please enter Y or N!');
+}
+
+var soozMovies = prompt ('Do I like to go to movies? Please responed with Y or N.').toUpperCase ();
+console.log ('The user thinks that Sooz '+ soozMovies + ' movies');
+
+if (soozMovies= 'Y') {
+    alert ('Yes I do like to go to movies.');
+
+}   else if (soozMovies== 'N') 
+{   alert ('Nope! See the pictures below.');
+}   
+    else  {
+    alert ('Please enter Y or N!');
+}
 
 //ARRAYS- collections, store more than one value in a variable as an array, data structure
+//name a third cat for sooz
 
-//var arrayofDogs = ['Gary','Charlotte', 'Demi', 'Bird', 'Mr. Darcy'];
+var arrayofCatNames = ['Suzuki','Buell','Yamaha'];
+
+var catNames = prompt ('Sooz\'s cats are called Harley and Ducati. Pick a number, 0 - 2 to see what a third cat might be named')
+console.log ('User picked '+ catNames );
+if (catNames === 0){
+    alert ('Sooz would name her third cat '+ arrayofCatNames[0] );
+}   else if (catNames === 1) 
+{
+    alert ('Sooz would name her third cat '+ arrayofCaNames[1] );
+}   
+    else if (catNames === 2) 
+{   alert ('Sooz would name her third cat '+ arrayofCatNames[2] );
+}   
+    else {
+    alert ('Please pick only a whole number from 0 to 2');
+}
+
+alert ('Thanks for playing along!')
